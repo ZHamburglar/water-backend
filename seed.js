13029bf4cd4d5a036bd89efc1f98111add9d0e9a
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2016-10-24 13:38:22 -0500

    added the seed file with PH levels

---
+++ seed.js
@@ -0,0 +1,212 @@
+[
+{
+  brand: "AppSprings",
+  phLevel: 5,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Aquafina",
+  phLevel: 4,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Arrowhead",
+  phLevel: 7,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Crystal Geyser",
+  phLevel: 7,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Dasani",
+  phLevel: 4,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Deep Rock",
+  phLevel: 8,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Deerpark",
+  phLevel: 7,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Eden",
+  phLevel: 7.9,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "El Dorado",
+  phLevel: 7,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Essentia",
+  phLevel: 5,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Evamor",
+  phLevel: 8,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Evian",
+  phLevel: 7.5,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Fiji",
+  phLevel: 7.5,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Glaceau Fruit Water",
+  phLevel: 4,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Le Bleu",
+  phLevel: 4,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "The Mountain Valley",
+  phLevel: 5,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Nestle",
+  phLevel: 6.5,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Niagara",
+  phLevel: 5,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Ozarka",
+  phLevel: 5,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Penta",
+  phLevel: 6.7,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Perrier",
+  phLevel: 4,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Poland Spring",
+  phLevel: 5,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Resource",
+  phLevel: 6.6,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Glaceau Smartwater",
+  phLevel: 4,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "San Pellegrino",
+  phLevel: 4,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Austin Tap Water",
+  phLevel: 9.6,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Glaceau Vitamin Water",
+  phLevel: 4,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Voss",
+  phLevel: 5,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Whole Foods 365",
+  phLevel: 7.5,
+  cost: 3,
+  image: ,
+  __v: 0
+},
+{
+  brand: "Zephyrhills",
+  phLevel: 7.5,
+  cost: 2,
+  image: ,
+  __v: 0
+},
+]
