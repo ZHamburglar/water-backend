daf963d8d58ff0fe7d6b3887a2a3824bf9e3ed8b
Thomas Foster <thomasfoster90>
2016-10-26 15:41:10 -0500

    fixed slider effect

--- routes/waterbottles.js
+++ routes/waterbottles.js
@@ -9,9 +9,21 @@ router.use(function (req, res, next) {
 })
 
 router.get('/', function (req, res) {
+  console.log(req.query);
+  var minPhLevel = req.query.minPhLevel;
+  var minPhLevel = (typeof minPhLevel !== 'undefined') ?  minPhLevel : 1;
+  var maxPhLevel = req.query.maxPhLevel;
+  var maxPhLevel = (typeof maxPhLevel !== 'undefined') ?  maxPhLevel : 14;
+console.log(minPhLevel, "minPhLevel");
+console.log(maxPhLevel, "maxPhLevel");
+
+
+
   //console.log('route hit');
   //console.log('req', req);
-  Waterbottle.find({}).sort({[req.query.orderBy]: 1}).exec(function (err, waterbottles) {
+  Waterbottle.find({
+    phLevel:{$gte:minPhLevel, $lte:maxPhLevel},
+  }).sort({[req.query.orderBy]: 1}).exec(function (err, waterbottles) {
     if (err) {
       res.status(500).send()
     } else {
@@ -19,6 +31,5 @@ router.get('/', function (req, res) {
     }
   });
 });
-
 
 module.exports = router;
